f3ea899be3fa1e144060b4cb08f641bbf124865c
kafedra-iust <kafedra.iust@gmail.com>
2021-01-31 23:55:40 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+// імпортування класу Date зі стандартного пакету java.util
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] S) {
+        System.out.println("Hello, Java!");
+        LocalDate d = LocalDate.now();
+        System.out.println("Date: "+d.toString());
+    }
+}
